7d4620f7c13ff40b250a62df1ce0d2bc45b4e8f5
marijas20210485 <marijas181@gmail.com>
2026-02-09 00:27:13 +0100

    Ispravke na reactu

--- react/cinemanager/src/App.js
+++ react/cinemanager/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home.jsx";
+import Logon from './Components/Pages/Logon.jsx';
+import Register from './Components/Pages/Register.jsx';
+import Logoff from './Components/Pages/Logoff.jsx';
+import Movies from './Components/Pages/Movies.jsx';
+import AdminNewMovie from './Components/Pages/AdminNewMovie.jsx';
+import AdminMovieList from './Components/Pages/AdminMovieList.jsx';
+import AdminEditMovie from './Components/Pages/AdminEditMovie.jsx';
+import OneMovie from './Components/Pages/OneMovie.jsx';
 function App() {
+  window.sessionStorage.setItem("LOGIN", "0");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter className="App">
+    <Routes>
+      
+      <Route index element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/logon" element={<Logon></Logon>}></Route>
+      <Route path="/logoff" element={<Logoff></Logoff>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
+      <Route path="/movies" element={<Movies></Movies>}></Route>
+      <Route path="/movie/:movie_id" element={<OneMovie></OneMovie>}></Route>
+      <Route path="/adminnewmovie" element={<AdminNewMovie></AdminNewMovie>}></Route>
+      <Route path="/adminmovielist" element={<AdminMovieList></AdminMovieList>}></Route>
+      <Route path="/admineditmovie/:movie_id" element={<AdminEditMovie></AdminEditMovie>}></Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
